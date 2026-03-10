8860d19e3bc6068fb6a69bb995f6b2aff263cdf5
Matheusnelago <nelagomatheus@gmail.com>
2026-03-10 15:26:12 +0200

    quick fix

--- core/deployment_settings.py
+++ core/deployment_settings.py
@@ -4,7 +4,7 @@ from .settings import BASE_DIR
 import dj_database_url
 
 
-ALLOWED_HOSTS = [os.environ.get(['RENDER_EXTERNAL_HOSTNAME'])]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = [f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}"]
 DEBUG = False
 SECRET_KEY = os.environ.get(['SECRET_KEY'])
