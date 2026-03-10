ae521fb5c8146b666b58620d8eb7ff81a2a21744
Matheusnelago <nelagomatheus@gmail.com>
2026-03-10 16:04:23 +0200

    quick fix

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from app import views
 
 
 urlpatterns = [
+    path('', views.health_check, name='root'),
     path('admin/', admin.site.urls),
     path('api/', include('app.urls'))
 ]
