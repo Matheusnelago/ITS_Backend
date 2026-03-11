55d21da9380da89894a02cd413ae787a04664cfc
Matheusnelago <nelagomatheus@gmail.com>
2026-03-11 22:00:41 +0200

    quick fix

--- build.sh
+++ build.sh
@@ -8,6 +8,4 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations app
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --password $DJANGO_SUPERUSER_PASSWORD --email $DJANGO_SUPERUSER_EMAIL
-fi
+
